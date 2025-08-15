9112eae0d0b59db29abdfffe0886243d4ed404eb
Alex <puyaalexxx@gmail.com>
2025-08-15 14:10:12 +0300

    added tickets factory and seeding them

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
